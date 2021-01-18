43c39592a83345966ebcbf53465cc06fec4f1126
Andrew Han <andrewthehan@gmail.com>
2021-01-18 15:43:33 -0800

    Minor code clean up.

--- RegularPolygon.java
+++ RegularPolygon.java
@@ -1,77 +1,72 @@
 
-import java.awt.geom.Point2D;
 import java.awt.Color;
 import java.awt.Graphics;
-import java.io.File;
+import java.awt.geom.Point2D;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-public class RegularPolygon extends JLabel{
-	private static int WIDTH = 600;
-	private static int HEIGHT = 600;
-	private static double RATIO = 100;
-	private static int NUMBER_OF_SHAPES = 1000;
-	private static double INITIAL_RADIUS = 10;
+public class RegularPolygon extends JLabel {
+	private static final int WIDTH = 600;
+	private static final int HEIGHT = 600;
+	private static final double RATIO = 100;
+	private static final int NUMBER_OF_SHAPES = 1000;
+	private static final double INITIAL_RADIUS = 10;
+	private static final Color BACKGROUND_COLOR = Color.BLACK;
+	private static final Color FOREGROUND_COLOR = Color.WHITE;
+
 	private Point2D.Double[] points;
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		Point2D.Double center = new Point2D.Double(WIDTH / 2, HEIGHT / 2);
 
 		List<RegularPolygon> polygons = new ArrayList<RegularPolygon>();
 		double radius = INITIAL_RADIUS;
-		for(int i = 0; i < NUMBER_OF_SHAPES; i++){
+		for (int i = 0; i < NUMBER_OF_SHAPES; i++) {
 			polygons.add(RegularPolygon.make(center, radius, i + 3));
 			radius += RATIO / radius;
 		}
 
-		JFrame f = new JFrame("Butterfly");
-		f.setSize(WIDTH, HEIGHT);
-		f.setLocationRelativeTo(null);
-		f.getContentPane().setBackground(Color.BLACK);
-		f.setLayout(null);
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JFrame frame = new JFrame("Butterfly");
+		frame.setSize(WIDTH, HEIGHT);
+		frame.setUndecorated(true);
+		frame.setLocationRelativeTo(null);
+		frame.getContentPane().setBackground(BACKGROUND_COLOR);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		polygons.forEach(f::add);
+		polygons.forEach(frame::add);
 
-		f.setVisible(true);
+		frame.setVisible(true);
 	}
 
-	private RegularPolygon(Point2D.Double[] points){
+	private RegularPolygon(Point2D.Double[] points) {
 		setBounds(0, 0, WIDTH, HEIGHT);
 		this.points = points;
 	}
 
 	@Override
-	public void paintComponent(Graphics g){
-		g.setColor(Color.WHITE);
-		for(int i = 0; i < points.length; i++){
-			Point2D.Double p1 = points[i];
-			Point2D.Double p2 = points[(i + 1 == points.length) ? 0 : i + 1];
+	public void paintComponent(Graphics g) {
+		g.setColor(FOREGROUND_COLOR);
+		Arrays.stream(points).reduce(points[points.length - 1], (p1, p2) -> {
 			g.drawLine((int) p1.getX(), (int) p1.getY(), (int) p2.getX(), (int) p2.getY());
-		}
+			return p2;
+		});
 	}
 
-	public static RegularPolygon make(Point2D.Double center, double radius, int numberOfSides){
+	public static RegularPolygon make(Point2D.Double center, double radius, int numberOfSides) {
 		return new RegularPolygon(RegularPolygon.getRootsOfUnity(center, radius, numberOfSides));
 	}
 
-	private static Point2D.Double[] getRootsOfUnity(Point2D.Double center, double radius, int numberOfSides){
+	private static Point2D.Double[] getRootsOfUnity(Point2D.Double center, double radius, int numberOfSides) {
 		double constant = 2 * Math.PI / numberOfSides;
 		Point2D.Double[] toReturn = new Point2D.Double[numberOfSides];
-		for(int i = 0; i < numberOfSides; i++){
-			toReturn[i] = new Point2D.Double(center.getX() + Math.round(Math.sin(constant * i) * radius * 100.0) / 100.0, center.getY() + Math.round(Math.cos(constant * i) * radius * 100.0) / 100.0);
+		for (int i = 0; i < numberOfSides; i++) {
+			toReturn[i] = new Point2D.Double(center.getX() + Math.round(Math.sin(constant * i) * radius * 100.0) / 100.0,
+					center.getY() + Math.round(Math.cos(constant * i) * radius * 100.0) / 100.0);
 		}
 		return toReturn;
-	}
-
-	@Override
-	public String toString(){
-		StringBuilder builder = new StringBuilder();
-		builder.append("Regular Polygon: radius is " + points[0].getX());
-		Arrays.asList(points).forEach(p -> builder.append("\n (" + p.getX() + ", " + p.getY() + ")"));
-		return builder.toString();
 	}
 }
