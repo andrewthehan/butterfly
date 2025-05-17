cf9b851b2dbc14be5243ce8d8e0021df9935507c
Andrew Han <andrewthehan@gmail.com>
2025-05-16 20:27:05 -0700

    Fix the site on mobile. Remove the drawing functionality for now.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import { onMount } from "svelte";
   import { draw } from "svelte/transition";
 
   type Point = { x: number; y: number };
@@ -22,11 +21,17 @@
     Math.sqrt(initialRadius ** 2 + 2 * radiusRatio * i)
   );
 
-  let render = $state(false);
+  let isDrawing = $state(false);
+  let drawingTimeout = $state<ReturnType<typeof setTimeout> | null>(null);
 
-  onMount(() => {
-    render = true;
-  });
+  // Figure out how to make the svg show the butterfly even when it's scaled up/down
+  function startDrawing() {
+    isDrawing = true;
+
+    const delay = 1000 * Math.log(count + 1) + 1000;
+
+    drawingTimeout = setTimeout(() => (isDrawing = false), delay);
+  }
 </script>
 
 <svelte:head>
@@ -55,7 +60,7 @@
   }).join(" ")}
   {@const index = sideCount - initialSideCount}
   <polygon
-    transition:draw|global={{
+    in:draw|global={{
       duration: 1000 / (index + 1), // C / x
       delay: 1000 * Math.log(index + 1), // Integral of C / x = C * ln(x)
     }}
@@ -68,14 +73,21 @@
 {/snippet}
 
 <section>
-  <svg {height} {width} xmlns="http://www.w3.org/2000/svg">
-    <rect width="100%" height="100%" fill="black" />
-    {#if render}
-      {#each Array(count) as _, i}
-        {@render regularPolygon(center, radii[i], i + initialSideCount)}
-      {/each}
+  <div class="frame" style:width={width + "px"}>
+    {#if isDrawing}
+      <svg viewBox="0 0 {width} {height}" xmlns="http://www.w3.org/2000/svg">
+        <rect width="100%" height="100%" fill="black" />
+        {#each Array(count) as _, i}
+          {@render regularPolygon(center, radii[i], i + initialSideCount)}
+        {/each}
+      </svg>
+    {:else}
+      <img
+        src="https://raw.githubusercontent.com/andrewthehan/butterfly/master/static/butterfly-light.png"
+        alt="Butterfly"
+      />
     {/if}
-  </svg>
+  </div>
   <a
     class="label"
     href="https://github.com/andrewthehan/butterfly"
@@ -83,19 +95,20 @@
     rel="noopener noreferrer"
   >
     <h2>Andrew Han</h2>
-
     <b>Butterfly, 2015</b>
-
     <br />
     <br />
-
     <b><i>An artwork utilizing regular polygons</i></b>
   </a>
 </section>
 
 <style>
-  :global(html) {
+  :global(body) {
     background: #999;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    justify-content: center;
   }
 
   :root {
@@ -109,15 +122,18 @@
     flex-flow: column;
     justify-content: center;
     align-items: center;
+    width: calc(768px +);
+    max-width: calc(100vw - 8%);
   }
 
   section:after {
     content: "";
     position: absolute;
+    top: 0;
     width: var(--light-width);
     transform-origin: top center;
-    background: linear-gradient(0deg, transparent, rgba(255, 255, 255, 0.4));
-    height: 100%;
+    background: linear-gradient(to top, transparent, rgba(255, 255, 255, 0.4));
+    height: 768px;
     filter: blur(16px);
     animation: lightMovement var(--light-swing-speed) infinite alternate
       ease-in-out;
@@ -134,9 +150,12 @@
     }
   }
 
-  svg {
-    margin: 16px;
-    border: 16px solid;
+  .frame {
+    aspect-ratio: 1 / 1;
+    max-width: 100%;
+    margin: 2vmin;
+    margin-bottom: calc(2vmin + 10px);
+    border: 2vmin solid;
     border-top-color: #333;
     border-bottom-color: #333;
     border-right-color: black;
@@ -154,6 +173,12 @@
     100% {
       box-shadow: -10px 10px 4px rgba(0, 0, 0, 0.6);
     }
+  }
+
+  svg,
+  img {
+    width: 100%;
+    height: 100%;
   }
 
   .label {
